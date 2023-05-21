87cb455ce00239d565d2d52734d45e2ba411a380
Jacobo Osorio <zapatanosedon@gmail.com>
2023-05-21 14:34:01 -0500

    New DataBaseContext.cs for the database created

---
+++ Boleteria_JacoboOsorioZapata/Boleteria_JacoboOsorioZapata/DAL/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Boleteria_JacoboOsorioZapata.DAL.Entities;
+
+namespace Boleteria_JacoboOsorioZapata.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tickets> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tickets>().HasIndex(c => c.Id).IsUnique();
+        }
+    }
+}
