9c384ef0c42df397bf8e6a0275182ab519f62072
Jacobo Osorio <zapatanosedon@gmail.com>
2023-05-21 14:25:01 -0500

    New DAL and Entities file created, new entity Tickest.cs created

---
+++ Boleteria_JacoboOsorioZapata/Boleteria_JacoboOsorioZapata/DAL/Entities/Tickets.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boleteria_JacoboOsorioZapata.DAL.Entities
+{
+    public class Tickets
+    {
+        [Display(Name = "ID del ticket")]
+        [Key]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de uso")]
+        public DateTime UseDate { get; set; }
+
+        [Display(Name = "¿Se usó?")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Puerta de entrada")]
+        public string EntranceGate { get; set; }
+    }
+}
