b0bc133ad959ff469b628e931aceee4717dffbf9
Jacobo Osorio <zapatanosedon@gmail.com>
2023-05-22 00:13:38 -0500

    TicketsController.cs created in WebPage_Boleteria project and Program.cs edited to make HttpClient depency work

---
+++ Boleteria_JacoboOsorioZapata/WebPage_Boleteria/Controllers/TicketsController.cs
@@ -0,0 +1,24 @@
+﻿using Boleteria_JacoboOsorioZapata.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebPage_Boleteria.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public TicketsController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7088/api/Tickets/Get";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Tickets> tickets = JsonConvert.DeserializeObject<List<Tickets>>(json);
+            return View(tickets);
+        }
+    }
+}
